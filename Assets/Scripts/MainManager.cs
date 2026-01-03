7c908d1c334d046e28cedb4f644384d9d670ba7f
Dave Moeller <davemoeller00@gmail.com>
2026-01-03 18:55:27 -0500

    Update MainManager.cs
    Adjust points

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -14,7 +14,6 @@ public class MainManager : MonoBehaviour
     public GameObject GameOverText;
     private bool m_Started = false;
     private int m_Points;
-    //public MenuManager MenuManager;
     private bool m_GameOver = false;
     public string playerName; // new variable declared
     public string saveFile = "/savefile_game.json";
@@ -43,7 +42,7 @@ public class MainManager : MonoBehaviour
         const float step = 0.6f;
          int perLine = Mathf.FloorToInt(4.0f / step);
 
-        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        int[] pointCountArray = new[] { 1, 2, 3, 4, 5, 6 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
