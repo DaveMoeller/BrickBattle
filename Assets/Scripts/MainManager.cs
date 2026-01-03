88a9f9346efb8f4dadbe076acd6b6be2f0e231e1
Dave Moeller <davemoeller00@gmail.com>
2026-01-03 18:28:33 -0500

    Initial commit

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,173 @@
+// Data Persistence
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainManager : MonoBehaviour
+{
+    public Brick BrickPrefab;
+    public int LineCount = 6;
+    public Rigidbody Ball;
+    public Text ScoreText;
+    public Text bestScoreText;
+    public GameObject GameOverText;
+    private bool m_Started = false;
+    private int m_Points;
+    //public MenuManager MenuManager;
+    private bool m_GameOver = false;
+    public string playerName; // new variable declared
+    public string saveFile = "/savefile_game.json";
+    public static MainManager Instance;
+    public int bestScore = 0;
+    public string playerWithBestScore = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setup();
+        if (MenuManager.Instance != null)
+        {
+            playerName = MenuManager.Instance.playerName;
+        }
+        else
+        {
+            playerName = "Player 01";
+        }
+    }
+    private void Reset()
+    {
+        Setup();
+    }
+    void Setup()
+    {
+        const float step = 0.6f;
+         int perLine = Mathf.FloorToInt(4.0f / step);
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        for (int i = 0; i < LineCount; ++i)
+        {
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint);
+            }
+        }
+        LoadAllData();
+        if (MenuManager.Instance != null)
+        {
+            playerName = MenuManager.Instance.playerName;
+        }
+        ScoreText.text = playerName + $" Score : {m_Points}";
+        bestScoreText.text = "Best Score: " + playerWithBestScore + " : " + bestScore;
+    }
+    private void Update()
+    {
+        if (!m_Started)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_Started = true;
+                float randomDirection = Random.Range(-1.0f, 1.0f);
+                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
+                forceDir.Normalize();
+
+                Ball.transform.SetParent(null);
+                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+            }
+        }
+        else if (m_GameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Ball.transform.SetParent(ballParentTransform);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                LoadAllData();
+            }
+        }
+    }
+
+    void AddPoint(int point)
+    {
+        m_Points += point;
+        ScoreText.text = playerName + $" Score : {m_Points}";
+        if (m_Points > bestScore)
+        {
+            bestScore = m_Points;
+            playerWithBestScore = playerName;
+            //Best Score : Player Name : 0
+            bestScoreText.text = "Best Score: " + playerWithBestScore + " : " + bestScore;
+        }
+    }
+
+    public void GameOver()
+    {
+        m_GameOver = true;
+        GameOverText.SetActive(true);
+    }
+    [System.Serializable]
+    class SaveData
+    {
+        public string playerName;
+        public int bestScore = 0;
+        public string playerWithBestScore = "";
+    }
+    public void SaveAllData()
+    {
+        if (MenuManager.Instance.usePlayerPreferences)
+        {
+            //PlayerPrefs.SetString("playerName", playerName);
+            PlayerPrefs.SetString("playerWithBestScore", playerWithBestScore);
+            PlayerPrefs.SetInt("bestScore", bestScore);
+        }
+        else
+        {
+            SaveData data = new SaveData();
+            data.playerName = playerName;
+            data.bestScore = bestScore;
+            data.playerWithBestScore = playerWithBestScore;
+
+            string json = JsonUtility.ToJson(data);
+
+            File.WriteAllText(Application.persistentDataPath + saveFile, json);
+            //    loadButton.SetActive(true);
+        }
+    }
+    public void LoadAllData()
+    {
+        if (MenuManager.Instance.usePlayerPreferences)
+        {
+            playerName = PlayerPrefs.GetString("playerName");
+            playerWithBestScore = PlayerPrefs.GetString("playerWithBestScore");
+            bestScore = PlayerPrefs.GetInt("bestScore");
+        }
+        else
+        {
+
+            string path = Application.persistentDataPath + saveFile;
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+                //playerName = data.playerName;
+                bestScore = data.bestScore;
+                playerWithBestScore = data.playerWithBestScore;
+                bestScoreText.text = "Best Score: " + playerWithBestScore + " : " + bestScore;
+            }
+        }
+    }
+    public bool SaveFileExists()
+    {
+        string path = Application.persistentDataPath + saveFile;
+        return File.Exists(path);
+    }
+    public void ExitToMenu()
+    {
+        SaveAllData();
+        Debug.Log("Loading scene 0");
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,200 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Overlays;
+#endif
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+[DefaultExecutionOrder(1000)]
+public class MenuManager : MonoBehaviour
+{
+    public string playerName;
+    public string saveFile = "/savefile.json";
+    public TMP_InputField nameInputField;
+    public static MenuManager Instance;
+    public static GameObject MenuCanvas;
+    //public MenuManager Instance;
+    public List<string> previousPlayers = new List<string>();
+    public TMP_Dropdown previousPlayersDropdown;
+    [UnityEngine.Range(1, 30)]
+    public int maxSavedPreviousPlayers = 20;
+    public bool usePlayerPreferences = true;
+    //public TMPro.TMP_Text nameInputField;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+
+    //}
+    //public void Start()
+    //{
+    //    nameInputField = GetComponent<TMP_InputField>();
+    //    Debug.Log("nameInputField: " +  nameInputField.name);
+    //}
+    public void Awake()
+    {
+        Debug.Log("gameObject.name: " + gameObject.name);
+
+        //start of new code
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            MenuCanvas.SetActive(true);
+            LoadAllData();
+            return;
+        }
+        //end of new code
+
+        Instance = this;
+        MenuCanvas = this.gameObject;
+
+        //DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(Instance); // same as GameObject
+
+        LoadAllData();
+
+        if (nameInputField != null)
+        {
+            nameInputField.text = playerName;
+            //Assign previous players to dropdown
+            previousPlayersDropdown.ClearOptions();
+            previousPlayersDropdown.AddOptions(previousPlayers);
+        }
+        else
+        {
+            Debug.Log("Awake nameInputField is null");
+        }
+
+    }
+    public void AssignPreviousPlayer()
+    {
+        playerName = previousPlayers.ElementAt(previousPlayersDropdown.value);
+        Debug.Log("Chosen Name:" + playerName);
+        nameInputField.text = playerName;
+    }
+    public void SavePlayer()
+    {
+        if (nameInputField != null && nameInputField.text.Length > 0)
+        {
+            Debug.Log("Saving Player: " + nameInputField.text);
+            previousPlayers.Insert(0, nameInputField.text);
+            previousPlayersDropdown.ClearOptions();
+            previousPlayersDropdown.AddOptions(previousPlayers);
+
+            if (previousPlayersDropdown.options.Count > maxSavedPreviousPlayers)
+            {
+                previousPlayersDropdown.options.RemoveRange(maxSavedPreviousPlayers, previousPlayersDropdown.options.Count - maxSavedPreviousPlayers);
+            }
+        }
+    }
+    public void StartNew()
+    {
+        if (nameInputField != null)
+        {
+            playerName = nameInputField.text;
+            SaveAllData();
+        }
+        else
+        {
+            Debug.Log("nameInputField is null");
+        }
+
+        //Test
+        gameObject.SetActive(false);
+
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+    public void Exit()
+    {
+        //if (MenuManager.Instance == null)
+        //{
+        //    Debug.Log("MenuManager.Instance=null");
+        //}
+        //else
+        //{
+        //    Debug.Log("MenuManager.Instance=" + MenuManager.Instance);
+        //}
+
+        MenuManager.Instance.SaveAllData();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+    class SaveData
+    {
+        public string playerName;
+        public List<string> previousPlayers;
+    }
+
+    public void SaveAllData()
+    {
+        if (usePlayerPreferences)
+        {
+            SaveAllPreferences();
+        }
+        else
+        {
+            SaveData data = new SaveData();
+            string filePath = Application.persistentDataPath + saveFile;
+            data.playerName = playerName;
+            data.previousPlayers = previousPlayers;
+
+            string json = JsonUtility.ToJson(data);
+            Debug.Log($"Save File Path: {filePath}\nSaveData:\n{json}");
+            File.WriteAllText(filePath, json);
+            //    loadButton.SetActive(true);
+
+        }
+    }
+    public bool SaveFileExists()
+    {
+        string path = Application.persistentDataPath + saveFile;
+        return File.Exists(path);
+    }
+    public void LoadAllData()
+    {
+        if (usePlayerPreferences)
+        {
+            LoadAllPreferences();
+        }
+        else
+        {
+            string path = Application.persistentDataPath + saveFile;
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+                playerName = data.playerName;
+                previousPlayers = data.previousPlayers;
+                //previousPlayersDropdown.ClearOptions();
+                //previousPlayersDropdown.AddOptions(data.previousPlayers);
+            }
+        }
+    }
+    public void LoadAllPreferences()
+    {
+        //Load Preferences (cookie)
+        playerName = PlayerPrefs.GetString("playerName");
+        previousPlayers = PlayerPrefs.GetString("previousPlayers").Split(";").ToList();
+    }
+    public void SaveAllPreferences()
+    {
+        //Save Preferences (cookie)
+        PlayerPrefs.SetString("playerName", playerName);
+        PlayerPrefs.SetString("previousPlayers", string.Join(";", previousPlayers));
+    }
+}
