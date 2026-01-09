1de7218408050cd784f37a5b545d0c19afa27a38
Dave Moeller <davemoeller00@gmail.com>
2026-01-08 21:19:22 -0500

    Update Paddle.cs
    Paddle color

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -6,11 +6,70 @@ public class Paddle : MonoBehaviour
 {
     public float Speed = 2.0f;
     public float MaxMovement = 2.0f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        //Renderer Renderer = gameObject.GetComponent<Renderer>();
+        if (meshRenderer != null)
+        {
+            Debug.Log($"meshRenderer: {meshRenderer.name}");
+
+            //Debug.Log($"MainManager.MaterialGreen: {MainManager.Instance.MaterialGreen}");
+
+            //Material material = meshRenderer.material;
+            //meshRenderer.material
+            if (MainManager.Instance != null)
+            {
+                switch (MenuManager.Instance.CurrentLevel)
+                {
+                    case
+                        "Green":
+                        {
+                            meshRenderer.material = MainManager.Instance.MaterialGreen;
+                            break;
+                        }
+                    case
+                        "Purple":
+                        {
+                            meshRenderer.material = MainManager.Instance.MaterialPurple;
+                            break;
+                        }
+                    case
+                         "Chocolate":
+                        {
+                            meshRenderer.material = MainManager.Instance.MaterialChocolate;
+                            break;
+                        }
+                    case
+                         "Blue":
+                        {
+                            meshRenderer.material = MainManager.Instance.MaterialBlue;
+                            break;
+                        }
+                    case
+                         "Silver":
+                        {
+                            meshRenderer.material = MainManager.Instance.MaterialSilver;
+                            break;
+                        }
+                    case
+                         "Gold":
+                        {
+                            meshRenderer.material = MainManager.Instance.MaterialGold;
+                            break;
+                        }
+                    default:
+                        meshRenderer.material = MainManager.Instance.MaterialGreen;
+                        break;
+                }
+            }
+            else
+            {
+                Debug.LogError("MainManager.Instance is null");
+            }
+        }
     }
 
     // Update is called once per frame
