52e6fab9ba84e64afa317ffcd9ab6d2ed6294195
Dave Moeller <davemoeller00@gmail.com>
2026-01-12 12:40:46 -0500

    Update Ball.cs
    Stop on game over

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -11,26 +11,38 @@ public class Ball : MonoBehaviour
     {
         m_Rigidbody = GetComponent<Rigidbody>();
     }
-    
+
     private void OnCollisionExit(Collision other)
     {
         var velocity = m_Rigidbody.linearVelocity;
-        
-        //after a collision we accelerate a bit
-        velocity += velocity.normalized * 0.01f;
-        
-        //check if we are not going totally vertically as this would lead to being stuck, we add a little vertical force
-        if (Vector3.Dot(velocity.normalized, Vector3.up) < 0.1f)
+        //ToDo: Count bricks and stop ball if no more
+        GameObject[] foundObjects;
+        foundObjects = GameObject.FindGameObjectsWithTag("Brick");
+        if (foundObjects.Length == 0)
         {
-            velocity += velocity.y > 0 ? Vector3.up * 0.6f : Vector3.down * 0.6f;
+            velocity = new Vector3 (0, 0, 0);
         }
-
-        //max velocity
-        if (velocity.magnitude > 3.0f)
+        else
         {
-            velocity = velocity.normalized * 3.0f;
+
+            //after a collision we accelerate a bit
+            velocity += velocity.normalized * 0.01f;
+
+            //check if we are not going totally vertically as this would lead to being stuck, we add a little vertical force
+            if (Vector3.Dot(velocity.normalized, Vector3.up) < 0.1f)
+            {
+                velocity += velocity.y > 0 ? Vector3.up * 0.6f : Vector3.down * 0.6f;
+            }
+
+            //max velocity
+            if (velocity.magnitude > 3.0f)
+            {
+                velocity = velocity.normalized * 3.0f;
+            }
         }
 
         m_Rigidbody.linearVelocity = velocity;
+        //ToDo: Count bricks and stop ball if no more
+        //ToDo: Display "You Won <name>"
     }
 }
