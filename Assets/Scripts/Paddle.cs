3b8f0862cc8876ba7d840df27c67457a69851b43
Dave Moeller <davemoeller00@gmail.com>
2026-03-11 12:43:55 -0400

    Cleanup

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Windows;
 
 public class Paddle : MonoBehaviour
 {
